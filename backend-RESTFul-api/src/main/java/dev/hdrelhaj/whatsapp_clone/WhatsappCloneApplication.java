8d321d79b41d2e7fef85f03ac475ebeb8250385c
AbdelrehmanElhaj <hdr333@gmail.com>
2025-03-17 02:34:30 +0000

    configuring the security of the application

--- backend-RESTFul-api/src/main/java/dev/hdrelhaj/whatsapp_clone/WhatsappCloneApplication.java
+++ backend-RESTFul-api/src/main/java/dev/hdrelhaj/whatsapp_clone/WhatsappCloneApplication.java
@@ -3,6 +3,15 @@ package dev.hdrelhaj.whatsapp_clone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main class for the application
+ * <p>
+ * This class is the entry point of the application
+ * </p>
+ *
+ * @author Abdelrehman Elhaj
+ */
+
 @SpringBootApplication
 public class WhatsappCloneApplication {
 
