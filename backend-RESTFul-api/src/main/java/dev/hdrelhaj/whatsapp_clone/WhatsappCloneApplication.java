78fd3bdf35134d8c92f7859de564ab4f86e964ff
AbdelrehmanElhaj <hdr333@gmail.com>
2025-03-18 00:59:30 +0000

    security scheme added to utlize by keycloak as OpenApi needed

--- backend-RESTFul-api/src/main/java/dev/hdrelhaj/whatsapp_clone/WhatsappCloneApplication.java
+++ backend-RESTFul-api/src/main/java/dev/hdrelhaj/whatsapp_clone/WhatsappCloneApplication.java
@@ -2,6 +2,13 @@ package dev.hdrelhaj.whatsapp_clone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 /**
  * Main class for the application
@@ -13,6 +20,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@EnableJpaAuditing
+@SecurityScheme(
+		name = "keycloak",
+		type = SecuritySchemeType.OAUTH2,
+		bearerFormat = "JWT",
+		scheme = "bearer",
+		in = SecuritySchemeIn.HEADER,
+		flows = @OAuthFlows(
+				password = @OAuthFlow(
+						authorizationUrl = "http://localhost:9090/realms/whatsapp-clone/protocol/openid-connect/auth",
+						tokenUrl = "http://localhost:9090/realms/whatsapp-clone/protocol/openid-connect/token"
+				)
+		)
+)
 public class WhatsappCloneApplication {
 
 	public static void main(String[] args) {
